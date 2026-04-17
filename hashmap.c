b65416ca8e73a8bde739ed9168e3f5e15bdd33b4
Your Name <you@example.com>
2026-04-17 23:44:54 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -152,6 +152,19 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
+    if(map == NULL) return NULL;
+
+    long pos = map->current + 1;
+    if(pos >= map->capacity) pos = 0;
+    
+    while(pos != map->current){
+        if(map->buckets[pos] != NULL && map->buckets[pos]->key != NULL){
+            map->current = pos;
+            return map->buckets[pos];
+        }
+        pos++;
+        if(pos >= map->capacity) pos = 0;
+    }
 
     return NULL;
 }
