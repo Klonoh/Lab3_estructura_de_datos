2a5ed9516bde14e12ba0ad1a977f093da8a30bad
Your Name <you@example.com>
2026-04-17 18:24:00 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -79,7 +79,7 @@ void insertMap(HashMap * map, char * key, void * value) {
     else{
         while(map->buckets[pos] != NULL || map->buckets[pos]->key != NULL){
             pos++;
-            if(pos == map->capacity) pos = 0;
+            if(pos >= map->capacity) pos = 0;
         }
         map->buckets[pos] = nuevo;
         map->size++;
